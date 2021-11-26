d6db2699372e5fb8dfebc37d2514379e4c93a5ee
max <max.guntzel@voffice.com.br>
2021-11-26 08:13:44 -0300

    initial commit

---
+++ src/main/java/com/raspberryawards/raspberry_awards/RaspberryAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.raspberryawards.raspberry_awards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryAwardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberryAwardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raspberryawards/raspberry_awards/RaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspberryawards.raspberry_awards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryAwardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
